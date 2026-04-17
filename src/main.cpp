dcff241f5c0cd104218d3c8f01a0e7abe58adb65
sabxio60-oss <sabxio60@gmail.com>
2026-04-17 14:53:17 +0200

    Created my first window, and added RGB Coloring to the text.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,23 @@
 
 int main()
 {
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(800, 450, "window name");
 	
-	std::cout << "hello world" << std::endl;
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawText("My first window!", 190, 200, 20, 
+			{255,0,200,255}
+			);
+
+		EndDrawing();
+	
+	}
+
+	CloseWindow();
+
 	return 0;
 }
