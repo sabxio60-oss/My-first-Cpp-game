ea3489fed80d233a8f70cacb775d661d37f4fc30
sabxio60-oss <sabxio60@gmail.com>
2026-04-17 20:23:13 +0200

    First windows in the imgui

--- src/main.cpp
+++ src/main.cpp
@@ -9,33 +9,79 @@ int main()
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 450, "window name");
 	
+	#pragma region imgui
 	rlImGuiSetup(true);
+
+	//Making the font bigger
+	ImGuiIO &io = ImGui::GetIO();
+	io.ConfigFlags = ImGuiConfigFlags_DockingEnable;
+	io.FontGlobalScale = 2.5;
+	#pragma endregion
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-				
-		rlImGuiBegin();
 		
+	#pragma region imgui
+		rlImGuiBegin();
+		//Docking settings
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+		ImGui::PopStyleColor(2);
+	#pragma endregion	
+
+
 		DrawRectangle(50, 50, 100, 100, {0, 255, 0, 127});
 		DrawRectangle(75, 75, 100, 100, { 255, 0, 0, 127 });
 
+	#pragma region imgui windows
 		ImGui::Begin("test");
 
 		ImGui::Text("hello");
-		ImGui::Button("button");
+
+		if(ImGui::Button("button")) 
+		{
+			std::cout << "Text\n";
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("button##2"))
+		{
+			std::cout << "Second button\n";
+		}
+
+		ImGui::PushID(2);
+
+		if (ImGui::Button("button##2"))
+		{
+			std::cout << "Thrid button\n";
+		}
+		ImGui::PopID();
+		
+
+		ImGui::End();
+
+		ImGui::Begin("second window");
+
+		ImGui::Text("hello");
+		ImGui::Separator();
+		ImGui::NewLine();
+		static float a = 0;
+		ImGui::SliderFloat("slider", &a, 0, 1);
 
 		ImGui::End();
 
 
 		rlImGuiEnd();
-
+	#pragma endregion
 		EndDrawing();
 		
 		
 	}
+	#pragma region imgui
 	rlImGuiShutdown();
+	#pragma endregion
 
 	CloseWindow();
 	
