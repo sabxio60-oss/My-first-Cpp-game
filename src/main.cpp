ad663f6bd72bf8b1dfc3f320d30a2a366a85663b
sabxio60-oss <sabxio60@gmail.com>
2026-04-17 18:21:39 +0200

    first imgui window

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
 #include <raylib.h>
 
+#include <imgui.h>
+#include <rlImGui.h>
+
 int main()
 {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 450, "window name");
 	
+	rlImGuiSetup(true);
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 				
-				
+		rlImGuiBegin();
+		
 		DrawRectangle(50, 50, 100, 100, {0, 255, 0, 127});
 		DrawRectangle(75, 75, 100, 100, { 255, 0, 0, 127 });
+
+		ImGui::Begin("test");
+
+		ImGui::Text("hello");
+		ImGui::Button("button");
+
+		ImGui::End();
+
+
+		rlImGuiEnd();
 
 		EndDrawing();
 		
 		
 	}
+	rlImGuiShutdown();
+
 	CloseWindow();
 	
 
